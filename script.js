c4b83e9fc6404312ff3fe0a0861e1960a39c377d
EssenceCode <123456789+odin@users.noreply.github.com>
2023-07-19 11:27:12 +0800

    added a function to render my board array

--- script.js
+++ script.js
@@ -9,61 +9,40 @@ const PlayerFactory = (name,marker) => {
 const playerOne = PlayerFactory('John','X');
 const playerTwo = PlayerFactory('Jane', 'O');
 
-const documentMock = (() => ({
-   querySelector: (selector) => ({
-    textContent: null
-   })
-}))();
+const gameBoard = (() => {
+    let board = new Array(9).fill('');
 
-const gameBoard = ((doc) => {
-    let board = new Array(9).fill('X');
-    //create a grid of divs
-   const createGrid = function(selector) {
-    for(let i = 0; i < board.length; i++) {
-        let container = doc.querySelector(selector);
-        const sqr = document.createElement('div');
-        sqr.classList.add('sqr');
-        sqr.setAttribute('sqr-index', i);
-        container.appendChild(sqr);
+    const placeMarker = (index,marker) => {
+         deleteItem = 1;
+         board.splice(index,deleteItem,marker);
+         console.log(board)
     }
-   }
-   const placeMarker = (index,marker) => {
-        deleteItem = 1;
-        board.splice(index,deleteItem,marker);
-        console.log(board)
-   }
-   
-   const getBoard = () => {
-        return {...board}
-   }
-    return {createGrid, placeMarker, getBoard,board}
-})(document || documentMock);
-
-gameBoard.createGrid('.game-container');
-// Gameboard.placeMarker(0,playerOne.getMarker())
+    
+     return {board, placeMarker}
+})();
 
 const displayController = (() => {
-    // ...
-    const getSqr = () => {
-        const sqr = document.querySelectorAll('.sqr')
+    const _container = document.querySelector('.game-container');
+   
+    for(let i = 0; i < 9; i++) {
+        const sqr = document.createElement('div');
+        sqr.classList.add('sqr');
+        sqr.setAttribute('index', i);
+       
+        _container.appendChild(sqr);
+    }
+   
 
-        sqr.forEach(val => val.addEventListener('click',(e) =>{
-            const sqrDiv = e.target;
-            const sqrAtt = sqrDiv.getAttribute('sqr-index')
-            // console.log(sqrDiv)
-            sqrDiv.textContent = 'X'
-            console.log(sqrAtt)
-
-        }))
+    const renderBoard = (board) => {
+        const sqrDiv = document.querySelectorAll('.sqr');
+    
+        for (let i = 0; i < board.length; i++) {
+            const sqr = document.querySelectorAll('.sqr')
+            sqr[i].textContent = board[i]
+        }
     }
 
-    const displayMark = () => {
-        // const sqr = document.getAttribute('index')
-        // sqr
-    }
-
-    return {getSqr,displayMark}
-
+    return { renderBoard }
 })();
-displayController.getSqr()
-displayController.displayMark()
+
+displayController.renderBoard(gameBoard.board)
