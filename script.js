ea1b357bc8ce6559a67c37dc54124f6ffac570b2
EssenceCode <123456789+odin@users.noreply.github.com>
2023-07-22 09:39:31 +0800

    created a function to tie the board to the dom

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // create a function to create players
 const PlayerFactory = (name,marker) => {
-    getName = () =>  name;
+    getName = () => name;
     getMarker = () => marker;
 
     return {getName, getMarker};
@@ -10,13 +10,13 @@ const playerOne = PlayerFactory('Player One','X');
 const playerTwo = PlayerFactory('Player Two', 'O');
 
 const gameBoard = (() => {
-    let _board = new Array(9).fill('');
+    let _board= new Array(9).fill('');
     let getBoard = () => [..._board]
 
     const placeMarker = (index,marker) => {
          deleteItem = 1;
          _board.splice(index,deleteItem,marker);
-         console.log(_board);
+        //  console.log(board.getBoard());
          
     }
 
@@ -25,37 +25,51 @@ const gameBoard = (() => {
         return boardWithCellValues
     }
     
-     return {getBoard, placeMarker, printBoard}
-})();
-
-const displayController = (() => {
-    const _container = document.querySelector('.game-container');
-   
-    for(let i = 0; i < 9; i++) {
-        const sqr = document.createElement('div');
-        sqr.classList.add('sqr');
-        sqr.setAttribute('index', i);
-       
-        _container.appendChild(sqr);
-    }
-   
-
-    const renderBoard = (board) => {
-        for (let i = 0; i < board.length; i++) {
-            const sqr = document.querySelectorAll('.sqr');
-            sqr[i].textContent = board[i];
+    const checkWinner = () => {
+        if(
+            // vertical
+          (_board[0] === playerOne.getMarker() && _board[1] === playerOne.getMarker() && _board[2] === playerOne.getMarker()) ||
+          (_board[3] === playerOne.getMarker() && _board[4] === playerOne.getMarker() && _board[5] === playerOne.getMarker()) ||
+          (_board[6] === playerOne.getMarker() && _board[7] === playerOne.getMarker() && _board[8] === playerOne.getMarker()) ||
+          // horizontal
+          (_board[0] === playerOne.getMarker() && _board[3] === playerOne.getMarker() && _board[6] === playerOne.getMarker()) ||
+          (_board[1] === playerOne.getMarker() && _board[4] === playerOne.getMarker() && _board[7] === playerOne.getMarker()) ||
+          (_board[2] === playerOne.getMarker() && _board[5] === playerOne.getMarker() && _board[8] === playerOne.getMarker()) ||
+          // diagonal
+          (_board[0] === playerOne.getMarker() && _board[4] === playerOne.getMarker() && _board[8] === playerOne.getMarker()) 
+        ) {
+            // console.log(playerOne.getName());
+            console.log(`${playerOne.getName()} won`);
         }
-    }
+      
+        else if (
+                   // vertical
+          (_board[0] === playerTwo.getMarker() && _board[1] === playerTwo.getMarker() && _board[2] === playerTwo.getMarker()) ||
+          (_board[3] === playerTwo.getMarker() && _board[4] === playerTwo.getMarker() && _board[5] === playerTwo.getMarker()) ||
+          (_board[6] === playerTwo.getMarker() && _board[7] === playerTwo.getMarker() && _board[8] === playerTwo.getMarker()) ||
+          // horizontal
+          (_board[0] === playerTwo.getMarker() && _board[3] === playerTwo.getMarker() && _board[6] === playerTwo.getMarker()) ||
+          (_board[1] === playerTwo.getMarker() && _board[4] === playerTwo.getMarker() && _board[7] === playerTwo.getMarker()) ||
+          (_board[2] === playerTwo.getMarker() && _board[5] === playerTwo.getMarker() && _board[8] === playerTwo.getMarker()) ||
+          // diagonal
+          (_board[0] === playerTwo.getMarker() && _board[4] === playerTwo.getMarker() && _board[8] === playerTwo.getMarker()) 
+        ) {
+            console.log(`${playerTwo.getName()} won`);
+        }
+        else if (!_board.includes('')) {
+            console.log('draw')
+        }
+      
+      };
+     
 
-    return { renderBoard }
+     return {getBoard, placeMarker, printBoard, checkWinner}
 })();
-
-// displayController.renderBoard(gameBoard.getBoard());
 
 const gameController = (() => {
     const board = gameBoard;
 
-    const players = [
+    const player = [
         {
             name: playerOne.getName(),
             marker: playerOne.getMarker(),
@@ -66,14 +80,14 @@ const gameController = (() => {
         }
     ]
 
-    let activePlayers = players[0];
+    let activePlayers = player[0];
 
     const switchPlayers = () => {
-        if(activePlayers === players[0]) {
-            activePlayers = players[1];
+        if(activePlayers === player[0]) {
+            activePlayers = player[1];
         }
-        else if (activePlayers === players[1]) {
-            activePlayers = players[0];
+        else if (activePlayers === player[1]) {
+            activePlayers = player[0];
         }
     }
 
@@ -81,13 +95,56 @@ const gameController = (() => {
 
     const playGame = (index) => {
         console.log(
-            `${getActivePlayer().name} put his marker on column${index}`
+            `${getActivePlayer().name} put his marker on cell ${index}`
         )
-        
         board.placeMarker(index,getActivePlayer().marker)
+        board.checkWinner();    
+    
+        switchPlayers(); 
 
-        switchPlayers();    
     }
 
     return { getActivePlayer, playGame}
 })();
+
+const displayController = (() => {
+    const game = gameController;
+    const _boardDiv = document.querySelector('.board');
+    const _playerTurn = document.querySelector('.turn');
+
+   
+    const createGrid = () => {
+        for(let i = 0; i < 9; i++) {
+            const sqr = document.createElement('div');
+            sqr.classList.add('sqr');
+            sqr.setAttribute('index', i);
+        
+            _boardDiv.appendChild(sqr);
+        }    
+    };
+
+    const updateScreen = () => {
+         const activePlayer = game.getActivePlayer().name;   
+
+        _playerTurn.textContent = activePlayer;
+    }
+
+    const sqrClick = () => {
+        const sqr = document.querySelectorAll('.sqr');
+        sqr.forEach(cell => cell.addEventListener('click', (e) => {
+            const selectedSqr = e.target.getAttribute('index');
+            e.target.textContent = `${game.getActivePlayer().marker}`
+            updateScreen();
+            game.playGame(selectedSqr);
+        }));
+      
+    }
+
+    createGrid();
+    updateScreen();
+    sqrClick();
+ 
+})();
+
+// displayController.renderBoard(gameBoard.getBoard());
+
